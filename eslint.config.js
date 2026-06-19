8d2402887f97b585f7fab6947d621f6fec629287
davif <davif@prismcode.local>
2026-06-19 00:36:05 -0300

    fix: simplify release workflow to skip binary build, fix eslint ignores

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import tseslint from "typescript-eslint";
 import prettier from "eslint-config-prettier";
 
 export default defineConfig([
-  { ignores: ["**/dist/**", "**/node_modules/**", "**/generated/**"] },
+  { ignores: ["**/dist/**", "**/node_modules/**", "**/generated/**", "**/.next/**", "**/next-env.d.ts", "**/.bun/**"] },
   ...tseslint.configs.recommended,
   {
     rules: {
